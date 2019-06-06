d1427a3e5724226e00330e29178d1e74b41c8927
tingyi198 <tingyi198@gmail.com>
2019-06-06 15:32:27 +0800

    將文章呈現在前台畫面

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,50 @@
 const express = require('express');
 const router = express.Router();
 const firebaseAdminDb = require('../connections/firebase-admin');
+const moment = require('moment');
+const striptags = require('striptags');
 
-// const ref = firebaseAdminDb.ref('any');
-// ref.once('value', function (snap) {
-//   console.log(snap.val());
-// });
-
+const categoriesRef = firebaseAdminDb.ref('categories');
+const articlesRef = firebaseAdminDb.ref('articles');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+  let categories = {};
+  categoriesRef.once('value').then(function (snapshot) {
+    categories = snapshot.val();
+    return articlesRef.orderByChild('update_time').once('value');
+  }).then(function (snapshot) {
+    const articles = [];
+    snapshot.forEach(function (snapshotChild) {
+      if (snapshotChild.val().status === 'public') {
+        articles.push(snapshotChild.val());
+      }
+    });
+    articles.reverse();
+    console.log(articles);
+    res.render('index', {
+      articles,
+      categories,
+      striptags,
+      moment,
+    });
+  });
 });
 
-router.get('/post', function (req, res, next) {
-  res.render('post');
+router.get('/post/:id', function (req, res, next) {
+  const id = req.param('id');
+  let categories = {};
+  categoriesRef.once('value').then(function (snapshot) {
+    categories = snapshot.val();
+    return articlesRef.child(id).once('value');
+  }).then(function (snapshot) {
+    const article = snapshot.val();
+    res.render('post', {
+      categories,
+      article,
+      moment,
+    });
+  });
 });
 
 
